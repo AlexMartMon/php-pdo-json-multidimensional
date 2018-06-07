0ca5a51b7ecf724a113dc881f78ec9096df861d5
Alejandro Martín monteagudo <alejandro12-11@hotmail.com>
2018-06-07 12:44:33 +0200

    Add files via upload

---
+++ jsonpdo.php
@@ -0,0 +1,31 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=bdgranatapas", $username, $password);
+		$resultJson = array();
+		// set the PDO error mode to exception
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$conn -> exec("set names utf8");
+
+		$stmt = $conn->prepare("SELECT * FROM establecimiento"); 
+		$stmt->execute();
+		foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $dato){
+			$stmt2 = $conn->prepare("select tipo,imgCategoria from categoria c INNER JOIN estcat e on c.idCategoria = e.idCategoria where e.idEstablecimiento =".$dato['idEstablecimiento']);
+			$stmt2->execute();
+			if ($stmt2->rowCount() > 0){
+				$array = array();
+				foreach($stmt2->fetchAll(PDO::FETCH_ASSOC) as $dato2){
+					$array[] = $dato2;
+				}
+				$dato['categoria'] = $array;
+			}
+			array_push($resultJson,$dato);
+		}
+		echo json_encode($resultJson);
+    }catch(PDOException $e){
+		echo "Connection failed: " . $e->getMessage();
+    }
+	$conn = null;
+?>
